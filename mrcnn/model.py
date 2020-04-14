d5e43246c59535a5d153af54b13bfc60a8ceff5c
umar98 <42386653+umar98@users.noreply.github.com>
2020-04-14 17:46:35 +0500

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -123,7 +123,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block,
 
     x = KL.Convolution2D(nb_filter3, 1, 1, name=conv_name_base + '2c', bias=False)(x)
     x = KL.BatchNormalization(epsilon=eps, axis=bn_axis, name=bn_name_base + '2c')(x, training=train_bn)
-    x = KL.Scale(axis=bn_axis, name=scale_name_base + '2c')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2c')(x)
 
     x = KL.Add()([x, input_tensor])#x = KL.merge([x, input_tensor], mode='sum', name='res' + str(stage) + block)
     x = KL.Activation('relu', name='res' + str(stage) + block + '_relu')(x)
