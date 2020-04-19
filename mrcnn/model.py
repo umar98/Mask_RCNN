e094ca56b37523b9fadc97deb93076a5a0ab18a5
umar98 <42386653+umar98@users.noreply.github.com>
2020-04-19 23:19:26 +0500

    Update model.py
    Updated Model Checkpoint

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2353,7 +2353,7 @@ class MaskRCNN():
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
                                         histogram_freq=0, write_graph=True, write_images=False),
-            keras.callbacks.ModelCheckpoint(self.checkpoint_path,
+            keras.callbacks.ModelCheckpoint(self.checkpoint_path,monitor='val_loss',save_best_only=True,period=3,
                                             verbose=0, save_weights_only=True),
         ]
 
